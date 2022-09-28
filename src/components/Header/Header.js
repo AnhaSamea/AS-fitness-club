ba616ae6db954c7659472cc777bfb55cdfa058ec
AnhaSamea <anhasamea707@gmail.com>
2022-09-28 23:22:42 +0600

    navbar created

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <nav>
+            {/* <i class="fa-regular fa-person-running"></i> */}
+            <h1>AS Fitness Club</h1>
+        </nav>
+    );
+};
+
+export default Header;
