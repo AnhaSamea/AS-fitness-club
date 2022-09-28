1974c431709a27cba791c179c6dede5a7e270309
AnhaSamea <anhasamea707@gmail.com>
2022-09-28 23:36:15 +0600

    margin and  padding in navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Header.css'
 const Header = () => {
     return (
-        <nav>
+        <nav className='mt-5 ms-3 p-2'>
             {/* <i class="fa-regular fa-person-running"></i> */}
             <h1>AS Fitness Club</h1>
         </nav>
