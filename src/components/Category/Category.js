11d4dfee67ef0a24b2594ab9afbe0f5e6db97ef0
AnhaSamea <anhasamea707@gmail.com>
2022-09-29 11:33:01 +0600

    displayed data

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Category.css'
 const Category = (props) => {
-    const { name, img, seller, price } = props.type;
+    const { name, img, age, time } = props.type;
     return (
         <div className='category'>
-           <img src={img} alt="" />
+            <img src={img} alt="" />
+            <h2>{name}</h2>
+            <p>For Age : {age}</p>
+            <p>Time required : {time}min</p>
+            <button>Add to list</button>
         </div>
     );
 };
--- src/components/Club/Club.js
+++ src/components/Club/Club.js
@@ -11,12 +11,12 @@ const Club = () => {
     },[])
     return (
         <div className='club-container'>
-          <div className="exercise-container">
+          <div className='exercise-container'>
                 {
                     types.map(type=><Category key={type.id} type={type}  ></Category>)
                }
             </div>
-            <div className="profile-container">
+            <div className='profile-container'>
                 <h3>Profile Info</h3>
             </div>  
         </div>
